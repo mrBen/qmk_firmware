a1aada9b8a1d5e7eb9ffa2594004fa5d05e5d23d
Benjamin Collet <benjamin.collet@protonmail.ch>
2021-06-10 10:46:34 +0200

    Change alpha layer to bepo

--- keyboards/lily58/keymaps/mrben/keymap.c
+++ keyboards/lily58/keymaps/mrben/keymap.c
@@ -1,7 +1,8 @@
 #include QMK_KEYBOARD_H
+#include "keymap_bepo.h"
 
 enum layer_number {
-    _QWERTY = 0,
+    _BEPO = 0,
     _LOWER,
     _RAISE,
     _ADJUST,
@@ -9,26 +10,26 @@ enum layer_number {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* QWERTY
+    /* BEPO
      * ,-----------------------------------------.                    ,-----------------------------------------.
-     * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+     * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+     * |      |   B  |   É  |   P  |   O  |   È  |                    |   ^  |   V  |   D  |   L  |   J  |   Z  |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
-     * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
-     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+     * |      |   A  |   U  |   I  |   E  |   ,  |-------.    ,-------|   C  |   T  |   S  |   R  |   N  |   M  |
+     * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+     * |      |   À  |   Y  |   X  |   .  |   K  |-------|    |-------|   ’  |   Q  |   G  |   H  |   F  |   W  |
      * `-----------------------------------------/      /      \      \-----------------------------------------'
-     *                  | LAlt | LGUI |LOWER |  /Space /        \Enter \  |RAISE |BackSP| RGUI |
+     *                  |      | ESC  |Space |  / Tab  /        \Enter \  |BackSP|Delete|      |
      *                  |      |      |      | /      /          \      \ |      |      |      |
      *                  `--------------------''------'            '------''--------------------'
      */
-    [_QWERTY] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                 KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+    [_BEPO] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, BP_B,    BP_EACU, BP_P,    BP_O,    BP_EGRV,                   BP_DCIR, BP_V,    BP_D,    BP_L,    BP_J,    BP_Z,
+        XXXXXXX, BP_A,    BP_U,    BP_I,    BP_E,    BP_COMM,                   BP_C,    BP_T,    BP_S,    BP_R,    BP_N,    BP_M,
+        XXXXXXX, BP_AGRV, BP_Y,    BP_X,    BP_DOT,  BP_K,    XXXXXXX, XXXXXXX, BP_QUOT, BP_Q,    BP_G,    BP_H,    BP_F,    BP_W,
+                                   XXXXXXX, KC_ESC,  KC_SPC,  KC_TAB,  KC_ENT,  KC_BSPC, KC_DEL,  XXXXXXX
     ),
 
     /* LOWER
