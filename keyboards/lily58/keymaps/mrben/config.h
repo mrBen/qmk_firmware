7363091c508902f3acb8c087bf4a1c434f012f88
Benjamin Collet <benjamin.collet@protonmail.ch>
2021-06-10 11:27:57 +0200

    Add layers

--- keyboards/lily58/keymaps/mrben/config.h
+++ keyboards/lily58/keymaps/mrben/config.h
@@ -22,12 +22,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
